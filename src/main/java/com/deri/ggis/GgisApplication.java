05d57fda0c4fb14e2699579af1507b1e7bf68125
happywzy <1154365135@qq.com>
2021-03-29 15:58:19 +0800

    项目初始化

---
+++ src/main/java/com/deri/ggis/GgisApplication.java
@@ -0,0 +1,13 @@
+package com.deri.ggis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GgisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GgisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/deri/ggis/GgisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deri.ggis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GgisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
